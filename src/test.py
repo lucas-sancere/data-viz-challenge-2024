71d3cd2bdd466b101991dc800f07d0a82d0c8b98
th96r4ss <alonso-pinar_a@univ-corse.fr>
2024-09-17 21:50:51 +1000

    added tabs

--- src/test.py
+++ src/test.py
@@ -7,7 +7,7 @@ Created on Thu Sep 12 20:38:18 2024
 """
 
 import dash
-from dash import dcc
+from dash import dcc, Input, Output
 import dash_html_components as html
 import plotly.express as px
 import plotly.graph_objects as go
@@ -61,62 +61,132 @@ for feature in geojson_data3['features']:
 # Create a Dash application
 app = dash.Dash(__name__)
 
-# Layout of the app
 app.layout = html.Div([
-    html.H1("Risque sur le réseau electrique aérien"),
-    dcc.Graph(
-        id='geojson-map',
-        figure={
-            'data': [
-                # First scatter layer
-                go.Scattermapbox(
-                    lat=[point['lat'] for point in points],
-                    lon=[point['lon'] for point in points],
-                    mode='markers',
-                    marker=go.scattermapbox.Marker(
-                        size=10,
-                        color='blue'  # Set color for the first set of points
-                    ),
-                    text=[point['statut'] for point in points],
-                    name='BT aérien'
-                ),
-                # Second scatter layer
-                go.Scattermapbox(
-                    lat=[point['lat'] for point in points2],
-                    lon=[point['lon'] for point in points2],
-                    mode='markers',
-                    marker=go.scattermapbox.Marker(
-                        size=10,
-                        color='red'  # Set a different color for the second set of points
-                    ),
-                    text=[point['statut'] for point in points2],
-                    name='HTA aérien'
-                ),
-                go.Scattermapbox(
-                    lat=[point['lat'] for point in points3],
-                    lon=[point['lon'] for point in points3],
-                    mode='markers',
-                    marker=go.scattermapbox.Marker(
-                        size=10,
-                        color='orange'  # Set a different color for the second set of points
-                    ),
-                    text=[point['statut'] for point in points3],
-                    name='HTB aérien'
-                )
-            ],
-            'layout': go.Layout(
-                title="Réseau aérien",
-                mapbox_style="open-street-map",
-                mapbox=dict(
-                    zoom=7,
-                    center={"lat": 42.16, "lon": 9.13}  # Center map on the data
-                ),
-                height=800,
-                width=800
-            )
-        }
-    )
+    dcc.Tabs(id="tabs-example", value='tab-1', children=[
+        dcc.Tab(label='EDF', value='tab-1'),
+        dcc.Tab(label='Environnement', value='tab-2'),
+    ]),
+    html.Div(id='tabs-content')
 ])
+
+# Callback to render content based on selected tab
+@app.callback(
+    Output('tabs-content', 'children'),
+    Input('tabs-example', 'value')
+)
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.H1("Risque sur le réseau electrique aérien"),
+            dcc.Graph(
+                id='geojson-map',
+                figure={
+                    'data': [
+                        # First scatter layer
+                        go.Scattermapbox(
+                            lat=[point['lat'] for point in points],
+                            lon=[point['lon'] for point in points],
+                            mode='markers',
+                            marker=go.scattermapbox.Marker(
+                                size=10,
+                                color='blue'  # Set color for the first set of points
+                            ),
+                            text=[point['statut'] for point in points],
+                            name='BT aérien'
+                        ),
+                        # Second scatter layer
+                        go.Scattermapbox(
+                            lat=[point['lat'] for point in points2],
+                            lon=[point['lon'] for point in points2],
+                            mode='markers',
+                            marker=go.scattermapbox.Marker(
+                                size=10,
+                                color='red'  # Set a different color for the second set of points
+                            ),
+                            text=[point['statut'] for point in points2],
+                            name='HTA aérien'
+                        ),
+                        go.Scattermapbox(
+                            lat=[point['lat'] for point in points3],
+                            lon=[point['lon'] for point in points3],
+                            mode='markers',
+                            marker=go.scattermapbox.Marker(
+                                size=10,
+                                color='orange'  # Set a different color for the second set of points
+                            ),
+                            text=[point['statut'] for point in points3],
+                            name='HTB aérien'
+                        )
+                    ],
+                    'layout': go.Layout(
+                        title="Réseau aérien",
+                        mapbox_style="open-street-map",
+                        mapbox=dict(
+                            zoom=7,
+                            center={"lat": 42.16, "lon": 9.13}  # Center map on the data
+                        ),
+                        height=800,
+                        width=800
+                    )
+                }
+            )
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H1("Risques environnement"),
+            dcc.Graph(
+                id='geojson-map',
+                figure={
+                    'data': [
+                        # First scatter layer
+                        go.Scattermapbox(
+                            lat=[point['lat'] for point in points],
+                            lon=[point['lon'] for point in points],
+                            mode='markers',
+                            marker=go.scattermapbox.Marker(
+                                size=10,
+                                color='blue'  # Set color for the first set of points
+                            ),
+                            text=[point['statut'] for point in points],
+                            name='BT aérien'
+                        ),
+                        # Second scatter layer
+                        go.Scattermapbox(
+                            lat=[point['lat'] for point in points2],
+                            lon=[point['lon'] for point in points2],
+                            mode='markers',
+                            marker=go.scattermapbox.Marker(
+                                size=10,
+                                color='red'  # Set a different color for the second set of points
+                            ),
+                            text=[point['statut'] for point in points2],
+                            name='HTA aérien'
+                        ),
+                        go.Scattermapbox(
+                            lat=[point['lat'] for point in points3],
+                            lon=[point['lon'] for point in points3],
+                            mode='markers',
+                            marker=go.scattermapbox.Marker(
+                                size=10,
+                                color='orange'  # Set a different color for the second set of points
+                            ),
+                            text=[point['statut'] for point in points3],
+                            name='HTB aérien'
+                        )
+                    ],
+                    'layout': go.Layout(
+                        title="Réseau aérien",
+                        mapbox_style="open-street-map",
+                        mapbox=dict(
+                            zoom=7,
+                            center={"lat": 42.16, "lon": 9.13}  # Center map on the data
+                        ),
+                        height=800,
+                        width=800
+                    )
+                }
+            )
+        ])
 
 # Run the app
 if __name__ == '__main__':
